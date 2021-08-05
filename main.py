c7910ed2be37d233e971f9502d046f4e2e791a0e
BO <andypig9@gmail.com>
2021-08-05 22:42:18 +0800

    complete first step add comment

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from webdriver_manager.microsoft import EdgeChromiumDriverManager
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.common.action_chains import ActionChains
 import random
 
 url = 'https://acy.com/en/open-live-account'
@@ -13,51 +14,93 @@ driver = webdriver.Edge(edge_path)
 driver.get(url)
 
 
-# 定位參數
-class Keyword:
+# locators
+class Locator:
     country_list = ['China', 'Taiwan']
     title_sequence = [1, 2, 3, 4, 5]
     select_language_box = '/html/body/div[1]/div[1]/div[1]/div[2]/div[2]/div[2]/div[1]'
     select_language_name = '//*[@id="gatsby-focus-wrapper"]/div[1]/div[2]/div[2]/div[2]/div[2]/div/*/p[text() = ' \
                            '"English"] '
     select_language_confirm = '/html/body/div[1]/div[1]/div[1]/div[2]/div[3]/a'
-    account_type_box = '/html/body/div[1]/div[1]/div/div/div[1]/div/div/div/div/div[2]/div/div/div[' \
-                       '2]/div/div/form/div[1]/div/div/div[1] '
-    account_type_personal = '/html/body/div[1]/div[1]/div/div/div[1]/div/div/div/div/div[2]/div/div/div[' \
-                            '2]/div/div/form/div[1]/div/div/div[2]/div/li[1] '
-    country_name_box = '//*[@id="gatsby-focus-wrapper"]/div/div/div[1]/div/div/div/div/div[2]/div/div/div[' \
-                       '2]/div/div/form/div[2]/div/div/div/div/div[1]/div '
+    select_account_type_box = '//*[@id="gatsby-focus-wrapper"]/div/div/div[1]/div/div/div/div/div[2]/div/div/div[' \
+                              '2]/div/div/form/div[1]/div/div '
+    select_account_type_personal = '/html/body/div[1]/div[1]/div/div/div[1]/div/div/div/div/div[2]/div/div/div[' \
+                                   '2]/div/div/form/div[1]/div/div/div[2]/div/li[1] '
+    select_country_name_box = '//*[@id="gatsby-focus-wrapper"]/div/div/div[1]/div/div/div/div/div[2]/div/div/div[' \
+                              '2]/div/div/form/div[2]/div/div/div/div/div[1]/div '
     select_country = f'//*[@id="liWrapper"]/*[text()="{random.choice(country_list)}"]'
-    title_box = '/html/body/div[1]/div[1]/div/div/div[1]/div/div/div/div/div[2]/div/div/div[2]/div/div/form/div[' \
-                '3]/div/div/div[1] '
+    select_title_box = '/html/body/div[1]/div[1]/div/div/div[1]/div/div/div/div/div[2]/div/div/div[2]/div/div/form/div[' \
+                       '3]/div/div/div[1] '
     select_title = f'/html/body/div[1]/div[1]/div/div/div[1]/div/div/div/div/div[2]/div/div/div[2]/div/div/form/div[' \
                    f'3]/div/div/div[2]/div/li[{random.choice(title_sequence)}]'
-    first_name_input = '/html/body/div[1]/div[1]/div/div/div[1]/div/div/div/div/div[2]/div/div/div[' \
+    input_first_name = '/html/body/div[1]/div[1]/div/div/div[1]/div/div/div/div/div[2]/div/div/div[' \
                        '2]/div/div/form/div[4]/div/input '
+    input_middle_name = '/html/body/div[1]/div[1]/div/div/div[1]/div/div/div/div/div[2]/div/div/div[' \
+                        '2]/div/div/form/div[5]/div/input '
+    input_last_name = '/html/body/div[1]/div[1]/div/div/div[1]/div/div/div/div/div[2]/div/div/div[' \
+                      '2]/div/div/form/div[6]/div/input '
+    input_email = '/html/body/div[1]/div[1]/div/div/div[1]/div/div/div/div/div[2]/div/div/div[2]/div/div/form/div[' \
+                  '7]/div/input '
+    select_country_code_box = '//*[@id="gatsby-focus-wrapper"]/div/div/div[1]/div/div/div/div/div[2]/div/div/div[' \
+                              '2]/div/div/form/div[8]/div[1]/div/div[1]/div '
+    # css selector
+    select_country_code = f'li[data-testid="undefined{random.randint(0, 241)}"]'
+    input_phone_number = '//*[@id="gatsby-focus-wrapper"]/div/div/div[1]/div/div/div/div/div[2]/div/div/div[' \
+                         '2]/div/div/form/div[8]/div[1]/div/div[2]/div/div/input '
+    button_get_otp_code = '/html/body/div[1]/div[1]/div/div/div[1]/div/div/div/div/div[2]/div/div/div[' \
+                          '2]/div/div/form/div[8]/div[2]/div/div/div[1]/button '
+    input_otp_code = '/html/body/div[1]/div[1]/div/div/div[1]/div/div/div/div/div[2]/div/div/div[2]/div/div/form/div[' \
+                     '8]/div[2]/div/div/div[2]/div/div/input '
+    button_next_page = '/html/body/div[1]/div[1]/div/div/div[1]/div/div/div/div/div[2]/div/div/div[' \
+                       '2]/div/div/form/button '
 
 
-def register():
+def register_personal_detail():
     WebDriverWait(driver, 5, 0.5). \
-        until(ec.element_to_be_clickable((By.XPATH, Keyword.select_language_confirm)))
-    # select language box will appear twice
-    for i in range(2):
-        driver.find_element_by_xpath(Keyword.select_language_box).click()
-        driver.find_element_by_xpath(Keyword.select_language_name).click()
-        driver.find_element_by_xpath(Keyword.select_language_confirm).click()
-        time.sleep(3)
+        until(ec.element_to_be_clickable((By.XPATH, Locator.select_language_confirm)))
+    # select language box
+    driver.find_element_by_xpath(Locator.select_language_box).click()
+    driver.find_element_by_xpath(Locator.select_language_name).click()
+    driver.find_element_by_xpath(Locator.select_language_confirm).click()
     # select account type
-    driver.find_element_by_xpath(Keyword.account_type_box).click()
-    driver.find_element_by_xpath(Keyword.account_type_personal).click()
+    driver.find_element_by_xpath(Locator.select_account_type_box).click()
+    driver.find_element_by_xpath(Locator.select_account_type_personal).click()
     # select country
-    driver.find_element_by_xpath(Keyword.country_name_box).click()
-    driver.find_element_by_xpath(Keyword.select_country).click()
+    driver.find_element_by_xpath(Locator.select_country_name_box).click()
+    driver.find_element_by_xpath(Locator.select_country).click()
     # select title
-    driver.find_element_by_xpath(Keyword.title_box).click()
-    driver.find_element_by_xpath(Keyword.select_title).click()
-    # input First name
-    driver.find_element_by_xpath(Keyword.first_name_input).clear()
-    driver.find_element_by_xpath(Keyword.first_name_input).send_keys('first name')
+    driver.find_element_by_xpath(Locator.select_title_box).click()
+    driver.find_element_by_xpath(Locator.select_title).click()
+    # input first name
+    driver.find_element_by_xpath(Locator.input_first_name).clear()
+    driver.find_element_by_xpath(Locator.input_first_name).send_keys('first name')
+    # input middle name
+    driver.find_element_by_xpath(Locator.input_middle_name).clear()
+    driver.find_element_by_xpath(Locator.input_middle_name).send_keys('middle name')
+    # input last name
+    driver.find_element_by_xpath(Locator.input_last_name).clear()
+    driver.find_element_by_xpath(Locator.input_last_name).send_keys('last name')
+    # input email
+    driver.find_element_by_xpath(Locator.input_email).clear()
+    driver.find_element_by_xpath(Locator.input_email).send_keys(f'a{random.randint(0, 1000)}a@a.com')
+    # hover on country code box
+    hover = ActionChains(driver).move_to_element(driver.find_element_by_xpath(Locator.select_country_code_box))
+    hover.perform()
+    # select country code
+    driver.find_element_by_xpath(Locator.select_country_code_box).click()
+    driver.find_element_by_css_selector(Locator.select_country_code).click()
+    # input phone number
+    driver.find_element_by_xpath(Locator.input_phone_number).send_keys(random.randint(100000000, 999999999))
+    # get code
+    driver.find_element_by_xpath(Locator.button_get_otp_code).click()
+    # input opt code
+    driver.find_element_by_xpath(Locator.input_otp_code).send_keys(random.randint(100000000, 999999999))
+    # wait next button clickable
+    WebDriverWait(driver, 5, 0.5). \
+        until(ec.element_to_be_clickable((By.XPATH, Locator.button_next_page)))
+    # click next button to next page
+    driver.find_element_by_xpath(Locator.button_next_page).click()
 
 
 if __name__ == '__main__':
-    register()
+    register_personal_detail()
